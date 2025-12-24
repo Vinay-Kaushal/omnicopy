d42848c9516cb72bf749e519f4872bf8b4c1a5a8
Vinay-Kaushal <vinaykaushal87439@gmail.com>
2025-12-24 19:19:38 +0530

    crud in campaign data

---
+++ main.py
@@ -0,0 +1,82 @@
+from random import randint
+from fastapi import FastAPI,HTTPException, Request ,Response
+from datetime import datetime
+from typing import Any
+app = FastAPI(root_path="/api/v1")
+
+@app.get("/")
+async def root():
+    return {"message": "hello folks"}
+
+data : Any = [
+   {
+      "campaign_id": 1,
+      "name" : "prevent mob lynching",
+      "due_date" : datetime.now(),
+      "created_at" : datetime.now()
+   },
+   {
+      "campaign_id": 2,
+      "name" : "black life matters",
+      "due_date" : datetime.now(),
+      "created_at" : datetime.now()
+       
+   }
+
+]
+
+
+@app.get("/campaigns")
+async def read():
+    return {"campaigns": data}
+
+
+@app.get("/campaigns/{id}",)
+async def readCampaigns (id : int):
+    for campaign in data :
+        if campaign.get("campaign_id") == id:
+            return {"campaign": campaign}
+        
+    raise HTTPException(status_code=404)
+    
+@app.post("/campaigns" , status_code=201)
+async def create_Campaign(body: dict[str,Any]):
+  
+
+    new = {
+        "campaign_id": randint(0,100),
+        "name" :  body.get("name"),
+        "due_date" :body.get("due_date"),
+        "created_at" : datetime.now()
+
+    }
+
+    data.append(new)
+    return{"campaign" : new}
+
+@app.put("/campaigns/{id}",)
+async def  update_campaign(id : int , body: dict[str, Any] ):
+    for index, campaign in enumerate(data):
+        if campaign.get("campaign_id")== id :
+
+            updated : Any = {
+            "campaign_id": id,
+            "name" :  body.get("name"),
+            "due_date" :body.get("due_date"),
+            "created_at" : campaign.get("created_at")
+
+            }
+
+            data[index] = updated
+            return {"campaign" : updated}
+    raise HTTPException(status_code=404)
+
+
+@app.delete("/campaigns/{id}",)
+async def  update_campaign(id : int ):
+    for index, campaign in enumerate(data):
+        if campaign.get("campaign_id")== id :
+
+            data.pop(index)
+            return Response(status_code=204)
+    raise HTTPException(status_code=404)
